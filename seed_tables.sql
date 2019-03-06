c7ae5c9e3fbcb92b51a30c3e4f5664b83949c123
Sindre Sivertsen <sjsivert@stud.ntnu.no>
2019-03-06 09:55:33 +0100

    init file

---
+++ seed_tables.sql
@@ -0,0 +1,7 @@
+/* Øvelsesgrupper */
+INSERT INTO Øvelsesgruppe (Navn) VALUES ('Bein')
+INSERT INTO Øvelsesgruppe (Navn) VALUES ('Armer')
+INSERT INTO Øvelsesgruppe (Navn) VALUES ('Rygg')
+INSERT INTO Øvelsesgruppe (Navn) VALUES ('Kjerne')
+
+INSERT INTO Aparat VALUES (NULL, 'Stang'), (NULL, 'Løse vekter'), (NULL, 'Leg press'), (NULL, 'Leg ')
