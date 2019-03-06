615d35e54ebb49b93d5d404ce77fde6dd261eab9
Sindre Sivertsen <sjsivert@stud.ntnu.no>
2019-03-06 10:02:04 +0100

    seed maskiner

--- seed_tables.sql
+++ seed_tables.sql
@@ -4,4 +4,7 @@ INSERT INTO Øvelsesgruppe (Navn) VALUES ('Armer')
 INSERT INTO Øvelsesgruppe (Navn) VALUES ('Rygg')
 INSERT INTO Øvelsesgruppe (Navn) VALUES ('Kjerne')
 
-INSERT INTO Aparat VALUES (NULL, 'Stang'), (NULL, 'Løse vekter'), (NULL, 'Leg press'), (NULL, 'Leg ')
+INSERT INTO Aparat VALUES (NULL, 'Stang', 'Stang som du løfter'), 
+(NULL, 'Løse vekter', 'Løse vekter som du løfter'), 
+(NULL, 'Leg press', 'Sitter på aparatet, løfter med beina'), 
+(NULL, 'Leg extension', 'Sitter på aparatet og løfter med beina'),
