cadd5d298c9f3382d66fabceddf343fcf8b34d5f
Eskild Hein Trøen <eskildht@stud.ntnu.no>
2019-03-14 11:50:56 +0100

    Changed name and added getObject in WorkoutCtrl

--- innlevering2/Main.java
+++ innlevering2/Main.java
@@ -2,13 +2,15 @@ package innlevering2;
 
 public class Main{
 
-  public ExerciseCtrl exerciseCtrl = new ExerciseCtrl();
+  public WorkoutCtrl workoutCtrl = new WorkoutCtrl();
 
   public static void main(String[] args) throws Exception{
     Main main = new Main();
     try{
-      Exercise exercise = new Exercise("2019-02-04 10:23", "70", "5", "8", "Veldig god innsats");
-      main.exerciseCtrl.saveObject(exercise);
+      Workout workout = new Workout("2019-02-04 10:23", "70", "5", "8", "Veldig god innsats");
+      main.workoutCtrl.saveObject(workout);
+      workout = main.workoutCtrl.getObject("1");
+      System.out.println(workout);
     }
     catch(IllegalArgumentException e){
       System.out.println(e.getLocalizedMessage());
